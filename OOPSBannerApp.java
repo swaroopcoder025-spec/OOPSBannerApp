043315727fd3a33a0636880cdc76923b3ab425d7
swaroop <swaroopgamer025@gmail.com>
2026-02-16 18:44:00 +0530

    Initial commit: add OOPSBannerApp Java file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,8 @@
+public class OOPSBannerApp {
+
+   
+    public static void main(String[] args) {
+        // Printing the literal text "OOPS" as required by UC1
+        System.out.println("OOPS");
+    }
+}

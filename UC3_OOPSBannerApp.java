a4fb6ea77ece77da80be4a23c3cc1ef0703eafb7
swaroop <swaroopgamer025@gmail.com>
2026-02-17 13:17:46 +0530

    UC3: Print Banner in UC3_OOPSBannerApp

---
+++ UC3_OOPSBannerApp.java
@@ -0,0 +1,11 @@
+public class UC3_OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println(String.join("","  ***     ***   *****   ***** "));
+        System.out.println(String.join(""," *   *   *   *  *    *  *     "));
+        System.out.println(String.join("","*     * *     * *    *  *     "));
+        System.out.println(String.join("","*     * *     * *****   ***** "));
+        System.out.println(String.join("","*     * *     * *           * "));
+        System.out.println(String.join(""," *   *   *   *  *           * "));
+        System.out.println(String.join("","  ***     ***   *       ***** "));
+    }
+}

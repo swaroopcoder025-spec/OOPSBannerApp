056994b44f7fca7a88cfaa492609dd7ceccf75af
swaroop <swaroopgamer025@gmail.com>
2026-03-02 21:44:40 +0530

    Added README and gitignore, cleaned repo

--- OOPSBannerApp.java
+++
@@ -1,8 +0,0 @@
-public class OOPSBannerApp {
-
-   
-    public static void main(String[] args) {
-        // Printing the literal text "OOPS" as required by UC1
-        System.out.println("OOPS");
-    }
-}
